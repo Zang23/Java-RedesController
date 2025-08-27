3eeb4a6c65721a54dbe00e20e38a5f50d3712242
Zang_23 <thiagoaraujo20005@gmail.com>
2025-08-27 00:06:34 -0300

    termiando as mudancas em linux do codigo

--- java_redes_controller/src/controller/RedesController.java
+++ java_redes_controller/src/controller/RedesController.java
@@ -19,7 +19,7 @@ public class RedesController {
 	public void ip(){
 		String os = os();
 		
-		if(os.contains("Windows 11")) {
+		if(os.contains("Windows")) {
 			leProc("ipconfig");
 		}else if(os.contains("Linux")) {
 			leProc("ifconfig");
@@ -32,6 +32,8 @@ public class RedesController {
 		
 		if(os.contains("Windows 11")) {
 			leProc("ping -4 -n 10 www.google.com.br");
+		}else if(os.contains("Linux")) {
+			leProc("ping -4 -c 10 www.google.com.br");
 		}
 	}
 	
@@ -54,37 +56,62 @@ public class RedesController {
 			InputStreamReader leitor = new InputStreamReader(fluxo);
 			BufferedReader buffer = new BufferedReader(leitor);
 			String linha = buffer.readLine();
-			while(linha != null) {
-				
-				if(linha.contains("Adaptador Ethernet")) {
-					System.out.println(linha);
+			String os = os();
+	
+			if(os.contains("Windows")) {
+				while(linha != null) {
+					
+					if(linha.contains("Adaptador Ethernet")) {
+						System.out.println(linha);
+					}
+					
+					if(linha.contains("IPv4")) {
+						String[] linhaIp = linha.split(" ");
+						System.out.println(linhaIp[linhaIp.length-1]);
+					}
+					
+					
+					if(linha.contains(" = ") && linha.contains("ms")) {
+						String[] linhaPing = linha.split(" ");
+						StringBuffer bufferLinha = new StringBuffer();
+						bufferLinha.append("Média: ").append(linhaPing[linhaPing.length-1]);
+						System.out.println(bufferLinha);
+					}
+					
+					linha = buffer.readLine();
 				}
+			}else if(os.contains("Linux")) {
 				
-				if(linha.contains("IPv4")) {
-					String[] linhaIp = linha.split(" ");
-					System.out.println(linhaIp[linhaIp.length-1]);
+				while(linha != null) {
+					if(linha.contains("inet ")) {
+						linha = linha.replaceAll("\\s+", " ");
+						String[] vetIpLinux = linha.split(" ");
+						System.out.println(vetIpLinux[2]);
+					}
+					
+					
+					
+					if(linha.contains("lo: ") || linha.contains("enp")) {
+						String[] vetRedeLinux = linha.split(" ");
+						System.out.println(vetRedeLinux[0]);
+						
+					}
+					
+					if(linha.contains("min/avg/max")) {
+						String[] vetPing = linha.split(" ");
+						String[] pings = vetPing[3].split("/");
+						
+						System.out.println("média: " + pings[1] + "ms");
+						
+					}
+					
+					
+					linha = buffer.readLine();
+
 				}
-				
-				
-				if(linha.contains(" = ") && linha.contains("ms")) {
-					String[] linhaPing = linha.split(" ");
-					StringBuffer bufferLinha = new StringBuffer();
-					bufferLinha.append("Média: ").append(linhaPing[linhaPing.length-1]);
-					System.out.println(bufferLinha);
-				}
-				
-				if(linha.contains("lo: ") || linha.contains("enp")) {
-					String[] vetRedeLinux = linha.split(" ");
-					System.out.println(vetRedeLinux[0]);
-				}
-				
-				if(linha.contains("inet ")) {
-					String[] vetIpLinux = linha.split(" ");
-					System.out.println(vetIpLinux[1]);
-				}
-				
-				linha = buffer.readLine();
 			}
+			
+			
 			buffer.close();
 			fluxo.close();
 			leitor.close();
